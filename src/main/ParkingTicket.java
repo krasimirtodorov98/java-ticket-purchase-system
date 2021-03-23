9cdfbddcc953e62376450991abd153e0d7f69494
Krasimir Todorov <krasimir.todorov98@gmail.com>
2021-03-24 00:35:30 +0200

    Added some functionality and business logic to new classes AirportParking & ParkingTicket

---
+++ src/main/ParkingTicket.java
@@ -0,0 +1,20 @@
+package main;
+
+import vehicles.Vehicle;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
+
+public class ParkingTicket {
+
+    private static int uniqueID=1000;
+
+    private int ID;
+    private Vehicle vehicle;
+    private LocalDateTime timeOfEntry;
+    private LocalDateTime timeOfExit;
+    private Duration durationOfParking; // = Duration.between(timeOfEntry,timeOfExit);
+
+
+}
